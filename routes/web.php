60c6a00efeda3247900c9fd2c4c42d766ffc194d
thisismegan <thisismegantoro@gmail.com>
2023-07-07 18:00:48 +0700

    templating layout sb admin

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
-
 
 // public routes
 Route::get('/', [AuthController::class, 'index']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('register', [RegisterController::class, 'create']);
 Route::post('register', [RegisterController::class, 'store']);
+
+//route admin
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index']);
+});
